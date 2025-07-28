de85d8dd917d5771bde2a33a8aed6d6a7887c987
lazizbek <lazizbekhasanov424@gmail.com>
2025-07-28 13:41:03 +0300

    js1

---
+++ script.js
@@ -0,0 +1,17 @@
+var name = prompt("Ismingizni kiriting")
+console.log(`Sizning ismingizni ${name}`);
+var age = +prompt("Yoshngizni kiriting")
+console.log(`Sizning yoshngizni ${age}`);
+
+var misol = +prompt("6+3=?")
+console.log(`6+3=${misol}`);
+var misol = +prompt("20-5=?")
+console.log(`20-5=${misol}`);
+var misol = +prompt("15*2=?")
+console.log(`15*2=${misol}`);
+var misol = +prompt("8/4=?")
+console.log(`8/4=${misol}`);
+var misol = +prompt("10%3=?")
+console.log(`10%3=${misol}`);
+
+alert(`Natijani kurish uchun konsolni oching`);
