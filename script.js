fde09c1a5a19ec783a0964df1ba08181ab113551
lazizbek <lazizbekhasanov424@gmail.com>
2025-07-28 14:10:52 +0300

    js1

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 var name = prompt("Ismingizni kiriting")
-console.log(`Sizning ismingizni ${name}`);
+console.log(`Mening Ismim ${name}`);
 var age = +prompt("Yoshngizni kiriting")
-console.log(`Sizning yoshngizni ${age}`);
+console.log(`Men ${age} yoshdaman`);
 
 var misol = +prompt("6+3=?")
-console.log(`6+3=${misol}`);
+console.log(`Misol 1: 6 + 3 = 9 Sizning Javob ${misol}`);
 var misol = +prompt("20-5=?")
-console.log(`20-5=${misol}`);
+console.log(`Misol 2: 20 - 5 = 15 Sizning Javob ${misol}`);
 var misol = +prompt("15*2=?")
-console.log(`15*2=${misol}`);
+console.log(`Misol 3: 15 * 2 = 30 Sizning Javob ${misol}`);
 var misol = +prompt("8/4=?")
-console.log(`8/4=${misol}`);
+console.log(`Misol 4: 8 / 4 = 15 Sizning Javob ${misol}`);
 var misol = +prompt("10%3=?")
-console.log(`10%3=${misol}`);
+console.log(`Misol 5: 10 % 3 = 1 Sizning Javob ${misol}`);
 
 alert(`Natijani kurish uchun konsolni oching`);
