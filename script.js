e6f38f7270ecdc9c425d97be4bcf18f3c9dd911b
lazizbek <lazizbekhasanov424@gmail.com>
2025-08-01 13:43:31 +0300

    js1

--- script.js
+++ script.js
@@ -3,15 +3,56 @@ console.log(`Mening Ismim ${name}`);
 var age = +prompt("Yoshngizni kiriting")
 console.log(`Men ${age} yoshdaman`);
 
-var misol = +prompt("6+3=?")
-console.log(`Misol 1: 6 + 3 = 9 Sizning Javob ${misol}`);
-var misol = +prompt("20-5=?")
-console.log(`Misol 2: 20 - 5 = 15 Sizning Javob ${misol}`);
-var misol = +prompt("15*2=?")
-console.log(`Misol 3: 15 * 2 = 30 Sizning Javob ${misol}`);
-var misol = +prompt("8/4=?")
-console.log(`Misol 4: 8 / 4 = 2 Sizning Javob ${misol}`);
-var misol = +prompt("10%3=?")
-console.log(`Misol 5: 10 % 3 = 1 Sizning Javob ${misol}`);
 
+var misol = +prompt("6+3=?")
+if (misol===9) {
+    console.log(`Sizning javob tugri: ${misol}`);
+} else {
+    console.log(`Sizning javob notugri: ${misol}.Tugri javob: 9`);
+}
+var misol = +prompt("20-5=?")
+if (misol===15) {
+    console.log(`Sizning javob tugri: ${misol}`);
+} else {
+    console.log(`Sizning javob notugri: ${misol}.Tugri javob: 15`); 
+}
+var misol = +prompt("15*2=?")
+if (misol===30) {
+    console.log(`Sizning javob tugri: ${misol}`);
+} else {
+    console.log(`Sizning javob notugri: ${misol}.Tugri javob: 30`); 
+}
+var misol = +prompt("8/4=?")
+if (misol===2) {
+    console.log(`Sizning javob tugri: ${misol}`);
+} else {
+    console.log(`Sizning javob notugri: ${misol}.Tugri javob: 2`); 
+}
+var misol = +prompt("10%3=?")
+if (misol===1) {
+    console.log(`Sizning javob tugri: ${misol}`);
+} else {
+    console.log(`Sizning javob notugri: ${misol}.Tugri javob: 1`); 
+}
 alert(`Natijani kurish uchun konsolni oching`);
+
+var age = +prompt("Yoshingiz nechida")
+if (age >= 151){
+    alert("Nimadur notugri ketdi")
+} else if (age >= 60) {
+    alert("Pensyonerga uxshaysiz agar tirik bulsangiz  :)")
+} else if (age >= 51) {
+    alert("Yaqinda pensiyaga chiqasiz")
+}  else if (age >= 50){
+    alert("Ishlashingiz kerak ")
+}  else if (age >= 19) {
+    alert("Ishlashingiz kerak")
+}  else if(age >= 18){
+    alert("Yoshsiz uqishingiz kerak ")
+}  else if (age >= 8) {
+    alert("Maktabga  borishingz kerak")
+}  else if (age >= 1) {
+    alert("Beshigda dam olishingiz kerak")
+}  else {
+    alert("Nimadur notugri ketdi")
+}
